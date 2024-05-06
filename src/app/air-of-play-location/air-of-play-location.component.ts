d54ad6cf0eef1c2ddb377898fb6db81c66585e32
Yanis3333 <yanisdezzaz33@gmail.com>
2024-05-06 16:20:18 +0200

    Ajout de la gestion des signaux

--- src/app/air-of-play-location/air-of-play-location.component.ts
+++ src/app/air-of-play-location/air-of-play-location.component.ts
@@ -1,7 +1,11 @@
 import {
   Component,
+  Signal,
+  WritableSignal,
+  computed,
   inject,
-  input
+  input,
+  signal
 } from '@angular/core';
 import {
   Input
@@ -21,6 +25,7 @@ import {
 import {
   DetailsComponent
 } from '../details/details.component';
+import { __values } from 'tslib';
 
 @Component({
   selector: 'app-air-of-play-location',
@@ -37,3 +42,35 @@ export class AirOfPlayLocationComponent {
   // airofplayLocation = input<Airofplaylocation>();
   // airofplayLocation = input.required<Airofplaylocation>();
 }
+
+// zone test Writable Signals
+// __________________________
+// const count = signal(0);
+// Signals are getter functions - calling them reads their value.
+// console.log('The count is: ' + count());
+
+// count.set(3);
+// console.log('Maintenant le compte est de : ' + count());
+
+// count.update(value=>value+1);
+// console.log('Maintenant le compte à bouger : ' + count());
+
+// zone test Signaux informatique (computed signals)
+// _________________________________________________
+// const count: WritableSignal<number> = signal(0);
+const doubleCount: Signal<number> = computed(() => count() * 2);
+// console.log('count : ' + count());
+
+// dépendance dynamiques
+const showCount = signal(false);
+const count = signal(0);
+// Avec computed() toujours appeler la méthode pour voir les résultat
+const conditionalCount = computed(() => {
+  if (showCount()) {
+    console.log(`The count is ${count()}.`);
+  } else {
+    console.log('Nothing to see here!');
+  }
+});
+// Appel de la méthode
+conditionalCount()
